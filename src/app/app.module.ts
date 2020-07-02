47ff6a5a9abbefa516251888c2a38e9692badafb
https://bpfsoftwaresolutions@dev.azure.com <humberto.mori@proximitycr.com>
2020-07-02 10:15:55 -0500

    preparing environment for cross field validator

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { ReactiveFormsModule} from '@angular/forms'
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeroFormReactiveComponent } from './hero-form-reactive/hero-form-reactive.component';
+import { HeroCrossFieldComponent } from './hero-cross-field/hero-cross-field.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroFormReactiveComponent
+    HeroFormReactiveComponent,
+    HeroCrossFieldComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hero-cross-field/hero-cross-field.component.ts
@@ -0,0 +1,51 @@
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { forbiddenNameValidator, emailValidator } from '../forbidden-name.directive';
+
+class Hero{
+  name: string;
+  alterEgo: string;
+  power: string;
+}
+
+@Component({
+  selector: 'app-hero-cross-field',
+  templateUrl: './hero-cross-field.component.html',
+  styleUrls: ['./hero-cross-field.component.css']
+})
+export class HeroCrossFieldComponent implements OnInit {
+
+  heroForm: FormGroup;
+  hero: Hero;
+  get name() { return this.heroForm.get('name'); }
+  get power() { return this.heroForm.get('power'); }
+  get alterEgo() { return this.heroForm.get('alterEgo'); }
+
+  constructor(private fb: FormBuilder) {
+    this.hero= new Hero();
+    this.hero.name="humberto",
+    this.hero.alterEgo="superman",
+    this.hero.power="k"
+  }
+
+  ngOnInit() {
+    this.heroForm = this.fb.group({
+      name : [this.hero.name,
+               [
+                 Validators.required,
+                 Validators.minLength(4),
+                 forbiddenNameValidator(/bob/i)
+               ]
+              ],
+      alterEgo: [this.hero.alterEgo,
+                 [
+                  Validators.required,
+                  Validators.minLength(4),
+                  forbiddenNameValidator(/bob/i)
+                 ]
+                ],
+      power: [this.hero.power, Validators.required]
+    })
+  }
+
+}
